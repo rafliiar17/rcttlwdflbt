7a3bd143d535007458837c989a84509ea50a67b1
rafzzz <raflii.arraafi17@gmail.com>
2025-02-03 16:31:42 +0000

    update workflow #7

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // Replace 'your-repo-name' with the actual name of your GitHub repository
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  base: "/rcttlwdflbt/",
 });
